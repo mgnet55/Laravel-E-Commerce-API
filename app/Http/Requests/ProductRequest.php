7fa442d66f3b279ea107fd06b906831be605eb96
Dreambig12 <sciencefi2@gmail.com>
2022-04-06 01:22:29 +0200

    creating product model

---
+++ app/Http/Requests/ProductRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProductRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+
+        $rules = [
+            'name'=>'required|string',
+            'description'=>'required|string',
+            'quantity'=>'required|numeric',
+            'price'=>'required|numeric',
+            'user_id'=>'required|exists:users,id',
+            'cat_id'=>'required|exists:categories,id'
+        ];
+        if($this->method() == 'POST')
+        {
+           $rules['image'] = 'required|image|mime:png,jpg,jpeg';
+        }
+
+        return $rules;
+        
+    }
+}
---
+++ database/migrations/2022_04_05_210214_create_products_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->text('name');
+            $table->text('description');
+            $table->integer('quantity');
+            $table->float('price');
+            $table->text('image');
+            $table->boolean('available');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')
+                  ->references('id')
+                  ->on('users')
+                  ->onupdate('cascade')
+                  ->onDelete('cascade');
+            $table->unsignedBigInteger('cat_id')->default(1);
+            $table->foreign('cat_id')
+                  ->references('id')
+                  ->on('categories')
+                  ->onupdate('cascade')
+                  ->onDelete('set default');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
