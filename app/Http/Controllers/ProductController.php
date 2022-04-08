34d80b0bc9af6340e5fe43fab83c33787adca1db
Dreambig12 <sciencefi2@gmail.com>
2022-04-08 22:34:20 +0200

    updating product permissions

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -11,10 +11,10 @@ class ProductController extends Controller
 
   function __construct()
   {
-       $this->middleware('permission:product-list|product-create|product-edit|product-delete', ['only' => ['index','show']]);
-       $this->middleware('permission:product-create', ['only' => ['create','store']]);
-       $this->middleware('permission:product-edit', ['only' => ['edit','update']]);
-       $this->middleware('permission:product-delete', ['only' => ['destroy']]);
+       $this->middleware('permission:index product|add product|edit product|delete product', ['only' => ['index','show']]);
+       $this->middleware('permission:add product', ['only' => ['create','store']]);
+       $this->middleware('permission:edit product', ['only' => ['edit','update']]);
+       $this->middleware('permission:delete product', ['only' => ['destroy']]);
     }
     /**
      * Display a listing of the resource.
