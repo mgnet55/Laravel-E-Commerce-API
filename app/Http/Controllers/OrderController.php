360a44cc2a1dbc5781f74074611557135515397f
unknown <ronnaw0111@gmail.com>
2022-04-09 17:03:01 +0200

    shippingcontroller and Order

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -1,1 +1,20 @@
+<?php
 
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Order;
+
+class OrderController extends Controller
+{
+    public function index()
+    {
+//        return Order::class->
+    }
+
+    public function getOrderDetails($id){
+
+        $order = Order::findOrFail($id);
+        return $order->orderItems;
+    }
+}
--- app/Http/Controllers/ShippingcompanyController.php
+++ app/Http/Controllers/ShippingcompanyController.php
@@ -1,2 +1,80 @@
+<?php
 
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\ShippingCompany;
+use App\Models\Order;
+class ShippingcompanyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    public function index()
+    {
+        $companies = ShippingCompany::all();
+        return $companies;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+    public function getOrders($id){
+
+        $shippingCompany = ShippingCompany::findOrFail($id);
+
+        $orders = $shippingCompany->orders;
+
+        foreach($orders as $order)
+        {
+            return Order::findOrFail($order->id)->orderItems;
+        }
+
+
+    }
 }
